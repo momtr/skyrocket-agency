e43c1984d2dafcd9fe81b3b5013fe4c6dc8379e1
momtr <mitterdorfer.moritz@gmail.com>
2021-02-22 17:12:01 +0100

    add jobs

--- app.js
+++ app.js
@@ -11,10 +11,18 @@ app.get('/contact', (req, res) => {
     res.sendFile(__dirname + '/views/contact.html');
 });
 
+app.get('/impress', (req, res) => {
+    res.sendFile(__dirname + '/views/impress.html')
+});
+
+app.get('/jobs', (req, res) => {
+    res.sendFile(__dirname + '/views/jobs.html')
+});
+
 app.post('/contact', (req, res) => {
     console.log(req.query);
     res.send('ok');
-})
+});
 
 app.listen(8080, () => {
     console.log('app started on port 8080');
